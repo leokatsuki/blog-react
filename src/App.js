6cb42ae21b582df97dd5c1ff6fd18f89c5be0262
Leonardo Katsuki <leok-12@hotmail.com>
2022-09-07 17:22:13 -0700

    header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import './App.css';
+import Header from 'pages/Header';
+import Footer from 'pages/Footer';
+import Home from 'pages/Home';
 
 function App() {
   return (
     <>
+    <Header></Header>
+    <Home></Home>
+    <Footer></Footer>
     </>
   );
 }
---
+++ src/pages/Footer/index.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return(
+        <>
+            <h1>Footer.js</h1>
+        </>
+    );
+}
+
+export default Footer; 
